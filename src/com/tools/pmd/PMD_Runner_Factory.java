ca108acfe913e02c9d924eb5721a4c4b228cee64
tgolnoft <tgolnoft@gmail.com>
2018-11-20 16:15:44 -0700

    Revert "working pmd default behaviout"
    This reverts commit 3c503e4e92e1d8e8c49e5a401c505d7d0eadb35f.

--- src/com/tools/pmd/PMD_Runner_Factory.java
+++ src/com/tools/pmd/PMD_Runner_Factory.java
@@ -1,12 +1,7 @@
 package com.tools.pmd;
 
-/**
- * Factory for PMD_Runner interface
- */
 public class PMD_Runner_Factory{
-    /*
-     * Returns a custom PMD runner with the supplied path
-     */
+
     public static PMD_Runner getPMDRunner(String pathToPMD){
         return new PMD_Custom_Runner(pathToPMD);
     }
