56d0ec2d9cc5bcc35dd0867a3188a18b4503cb49
James Niedfeldt <37161212+JamesNiedfeldt@users.noreply.github.com>
2018-12-01 11:59:35 -0700

    Revert "Pmd preferences"

--- src/com/tools/pmd/PMD_Preference_Panel.java
+++ src/com/tools/pmd/PMD_Preference_Panel.java
@@ -6,9 +6,6 @@ import com.bluejmanager.BlueJManager;
 import javax.swing.*;
 import java.awt.*;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
 /**
  * Manages PMD extension panel in BlueJ preferences..
  */
@@ -22,16 +19,12 @@ public class PMD_Preference_Panel implements PreferenceGenerator {
 
     /** The file containing checks for PMD to run */
     private JTextField pmdCheck;
-    
-    private JCheckBox defaultCheckbox;
 
     /** Path to PMD distribution */
     private static final String PROPERTY_PMD_PATH = "PMD.Path";
 
     /** Path to file with list of PMD checks to perform. */
     private static final String PROPERTY_PMD_CHECK = "PMD.check";
-
-    private static final String PROPERTY_PMD_DEFAULT = "PMD.default";
 
     /** Field width for text fields */
     private static final int FIELD_WIDTH = 40;
@@ -40,7 +33,6 @@ public class PMD_Preference_Panel implements PreferenceGenerator {
         PMDPanel = new JPanel();
         PMDPanel.setLayout(new GridLayout(2,1));
         final JPanel labelTextFieldPanel = new JPanel();
-
         final JPanel labelPanel = new JPanel();
         labelPanel.setLayout(new GridLayout(3, 1));
         labelPanel.add(new JLabel("DefaultPMD"));
@@ -50,11 +42,8 @@ public class PMD_Preference_Panel implements PreferenceGenerator {
 
         final JPanel fieldPanel = new JPanel();
         fieldPanel.setLayout(new GridLayout(3,1));
-
-        defaultCheckbox = new JCheckBox("");
-        defaultCheckbox.setSelected(true);
+        JCheckBox defaultCheckbox = new JCheckBox("");
         fieldPanel.add(defaultCheckbox);
-
         pmdPath = new JTextField(FIELD_WIDTH);
         fieldPanel.add(pmdPath);
         pmdCheck = new JTextField(FIELD_WIDTH);
@@ -63,11 +52,6 @@ public class PMD_Preference_Panel implements PreferenceGenerator {
 
         final JPanel buttonPanel = new JPanel();
         JButton button = new JButton("OK");
-        button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                saveValues();
-            }
-        });
         buttonPanel.add(button);
         PMDPanel.add(labelTextFieldPanel);
         PMDPanel.add(buttonPanel);
@@ -76,26 +60,19 @@ public class PMD_Preference_Panel implements PreferenceGenerator {
     }
 
     public void saveValues() {
-        BlueJManager manager = BlueJManager.getInstance();
-        manager.setExtensionPropertyString(PROPERTY_PMD_PATH, pmdPath.getText());
-        manager.setExtensionPropertyString(PROPERTY_PMD_CHECK, pmdCheck.getText());
-        manager.setExtensionPropertyString(PROPERTY_PMD_DEFAULT, Boolean.toString(defaultCheckbox.isSelected()) );
+        BlueJManager manger = BlueJManager.getInstance();
+        manger.setExtensionPropertyString(PROPERTY_PMD_PATH, pmdPath.getText());
+        manger.setExtensionPropertyString(PROPERTY_PMD_CHECK, pmdCheck.getText());
     }
 
     public void loadValues(){
         BlueJManager manger = BlueJManager.getInstance();
 
         String path = manger.getExtensionPropertyString(PROPERTY_PMD_PATH, "");
-        String check = manger.getExtensionPropertyString(PROPERTY_PMD_CHECK, "java-quickstart");
-        String defaultCB = manger.getExtensionPropertyString(PROPERTY_PMD_DEFAULT, "true");
-        
+        String check = manger.getExtensionPropertyString(PROPERTY_PMD_CHECK, "");
+
         pmdPath.setText(path);
         pmdCheck.setText(check);
-        defaultCheckbox.setSelected(Boolean.parseBoolean(defaultCB));
-    }
-
-    public void onChange(){
-        
     }
 
     public JPanel getPanel () {
